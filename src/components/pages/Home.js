8baebde253f16b65deac0fc23ea386bf7c6f2ab3
Davi-angelo <davi.magalhaes.pb@compasso.com.br>
2024-08-25 22:48:48 -0300

    arquivos iniciais

---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import styles from "./Home.module.css"
+
+function Home() {
+    return(
+        <div className={styles.page}>
+            <div className={styles.calculator}></div>
+        </div>
+    )
+}
+
+export default Home
